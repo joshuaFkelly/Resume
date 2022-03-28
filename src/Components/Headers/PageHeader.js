9ad349588e257ba1d95e4f9e2b2f8792f4099dcf
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-28 19:34:25 -0400

    now that i got tailwind working im trying to structure my page

--- src/Components/Headers/PageHeader.js
+++ src/Components/Headers/PageHeader.js
@@ -3,14 +3,14 @@ export const Header = ({ title }) => {
         <header>
 
             <div className="text-center">
-                <h1 className="text-3xl font-semibold text-center">{title}</h1>
+                <h1 className="text-3xl font-semibold">{title}</h1>
                 <p>9 Lea Way Palmetto, Ga 30268 (icon) 312-479-8425</p>
             </div>
 
-            <ul className=" flex flex-row">
+            <ul className=" flex flex-row justify-evenly">
 
                 <li>
-                    <a href="#">jdkpersonal2015@gmail.com</a>
+                    <a href="#">Email</a>
                 </li>
 
                 <li>
@@ -22,7 +22,7 @@ export const Header = ({ title }) => {
                 </li>
 
                 <li>
-                    <a href="#">My Portfolio</a>
+                    <a href="#">Portfolio</a>
                 </li>
 
             </ul>
