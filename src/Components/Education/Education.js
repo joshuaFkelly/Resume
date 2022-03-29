1633d4a9d7f43bd2ffdbeadf49bd2801642131c3
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-29 17:18:41 -0400

    spacing done for entire page. moving on to color

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -3,16 +3,16 @@ export const Education = () => {
         <section className="p-2">
             <h1 className="text-2xl font-semibold">EDUCATION</h1>
 
-            <article className="mb-2">
-                <p>May 2015</p>
-                <h3 className="text-l font-semibold">High School Diploma, North Gate High School</h3>
-                <p>Graduted on time with class of 2015</p>
+            <article className="mb-3">
+                <p className="m-1">May 2015</p>
+                <h3 className="text-l font-semibold m-1">High School Diploma, North Gate High School</h3>
+                <p className="m-1">Graduted on time with class of 2015</p>
             </article>
 
-            <article className="mt-2">
-                <p>November 2019</p>
-                <h3 className="text-l font-semibold">Full Stack Web Development, Georgia Tech</h3>
-                <p>Incredibly in-depth and fast paced bootcamp. Students were taught the latest technology behind creating websites from the ground up using both frontend and backend technologies.</p>
+            <article className="mt-3">
+                <p className="m-1">November 2019</p>
+                <h3 className="text-l font-semibold m-1">Full Stack Web Development, Georgia Tech</h3>
+                <p className="m-1">Incredibly in-depth and fast paced bootcamp. Students were taught the latest technology behind creating websites from the ground up using both frontend and backend technologies.</p>
             </article>
         </section>
     )
