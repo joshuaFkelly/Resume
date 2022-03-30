7f29b791c59ef0eed9a60ad2d5e646ca06bda2ad
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-29 21:12:35 -0400

    tomorrow i will work on making my code more concise. learning about state and how to map throgh data to create multiple elements

--- src/Components/Summary/Summary.js
+++ src/Components/Summary/Summary.js
@@ -2,7 +2,7 @@ export const Summary = () => {
     return (
 
         <section className="mt-5 text-start p-2 mx-auto">
-            <p className="mx-auto">Self-taught, full stack web developer specializing in MERN Stack technology. Experience working with teams of different sizes and able to efficiently work alone to complete tasks. Certified by Georgia Tech in Full Stack Flex Web Development.</p>
+            <p className="mx-auto">Self-taught, full stack web developer specializing in MERN Stack technology. Experience working with teams of different sizes. Proven to efficiently work alone to complete tasks. Certified by Georgia Tech in Full Stack Flex Web Development.</p>
         </section>
     )
 }
