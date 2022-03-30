3e2138809e5977c228a0fda11e979f78fac61a4a
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-29 20:24:31 -0400

    brought in icons, styled rest of page. going to add switch then start researching how to do this whole switch css thing

--- src/Components/Headers/PageHeader.js
+++ src/Components/Headers/PageHeader.js
@@ -1,31 +1,48 @@
+import { GoMarkGithub } from "react-icons/go";
+import { AiFillLinkedin } from "react-icons/ai";
+import { SiGmail } from "react-icons/si";
+import { CgWebsite } from "react-icons/cg";
+import ReactTooltip from 'react-tooltip';
+const copy = () => navigator.clipboard.writeText("jdkpersonal2015@gmail.com");
+
 export const Header = () => {
     return (
         <header className="p-4">
             <div className="text-center">
                 <h1 className="text-5xl font-bold text-center"><span className="text-gray-700 font-light">Joshua</span> <span className="font-extrabold">Kelly</span></h1>
-                <p className="m-1 text-gray-700 text-sm">9 Lea Way Palmetto, GA 30268</p>
-                <p className="m-1 text-gray-700 text-sm"><span className="icon"></span>312-479-8425</p>
+                <p className="mt-1 text-gray-700 text-sm">9 Lea Way Palmetto, GA 30268</p>
+                <p className="mt-1 text-gray-700 text-sm">312-479-8425</p>
             </div>
 
-            <ul className="flex flex-row justify-evenly m-1">
+            <ul className="flex flex-row justify-evenly mt-3">
 
                 <li>
-                    <a href="#">Email</a>
+                    <SiGmail size={40} data-tip="Copy to Clipboard" onClick={copy}></SiGmail>
+                    <ReactTooltip />
                 </li>
 
                 <li>
-                    <a href="https://github.com/joshuaFkelly" target={"_blank"}>Github</a>
+                    <GoMarkGithub size={40} data-tip="Github">
+                        <a href="https://github.com/joshuaFkelly" target={"_blank"}></a>
+                    </GoMarkGithub>
+                    <ReactTooltip />
                 </li>
 
                 <li>
-                    <a href="https://www.linkedin.com/in/joshua-kelly-482196185/" target={"_blank"}>LinkedIn</a>
+                    <AiFillLinkedin size={40} data-tip="LinkedIn">
+                        <a href="https://www.linkedin.com/in/joshua-kelly-482196185/" target={"_blank"}></a>
+                    </AiFillLinkedin>
+                    <ReactTooltip />
                 </li>
 
                 <li>
-                    <a href="#">Portfolio</a>
+                    <CgWebsite size={40} data-tip="Portfolio">
+                        <a href="#" target={"_blank"}></a>
+                    </CgWebsite>
+                    <ReactTooltip />
                 </li>
 
             </ul>
-        </header>
+        </header >
     )
 }
