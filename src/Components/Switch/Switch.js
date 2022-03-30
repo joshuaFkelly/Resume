853ff60e8f4f3f048c880a1d71592993fa64e2e2
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-30 16:25:30 -0400

    okay i added the switch. oi have no idea what im doing and i dont have time to dive in depth to learn. im just doing. i dont really like it

--- src/Components/Switch/Switch.js
+++ src/Components/Switch/Switch.js
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 
 export const Switch = () => {
+    const [toggle, setToggle] = useState(true)
+
+    const toggleClass = ' transform translate-x-6';
     return (
         //   Switch Container
-        <div
-            className="absolute top-0 right-0 w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer"
-        >
+        <div className="md:w-14 md:h-7 w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer"
+            onClick={() => {
+                setToggle(!toggle);
+            }}>
+
             {/* Switch */}
-            <div
-                className="bg-white h-5 w-5 rounded-full shadow-md"
-            ></div>
+            <div className={"bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform duration-300 ease-in-out" + (toggle ? null : toggleClass)}></div>
+
         </div>
     );
 }
